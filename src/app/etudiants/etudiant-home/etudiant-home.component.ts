8ebc530e65ee304208b7657018d320b69df749be
Franckie ANDRIAMALALA <80829683+FranckieAndria@users.noreply.github.com>
2024-04-10 19:55:38 +0300

    Static data for Home

--- src/app/etudiants/etudiant-home/etudiant-home.component.ts
+++ src/app/etudiants/etudiant-home/etudiant-home.component.ts
@@ -1,7 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 @Component({
   selector: 'app-etudiant-home',
+  standalone: true,
+  imports: [
+    MatGridListModule,
+    MatCardModule,
+  ],
   templateUrl: './etudiant-home.component.html',
   styleUrls: ['./etudiant-home.component.css']
 })
